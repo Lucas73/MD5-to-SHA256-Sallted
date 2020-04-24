eca798dca7e2145be8f1cc42324f3e3b76095ac2
Lucas <lucas@Lenovo.local>
2020-04-24 10:22:29 +0200

    last changes

--- convert.py
+++ convert.py
@@ -5,17 +5,14 @@ import hashlib
 import bcrypt
 from secrets import *
 
-with open('PASSWORDS.md','r') as hash:
-    lineas = hash.readlines()
-
 hashList = []
 clearHashList = []
 
-for element in lineas:
-    hashList.append(element.strip())
+with open('PASSWORDS.md','r') as hash:
+    for element in hash:
+        hashList.append(element.rstrip())
 
-outputClearHashes = open("outputClearHashes.txt","w") 
-index = 0
+plain = open("plain.txt","w") 
 for element in hashList:
   url = 'https://md5decrypt.net/en/Api/api.php'
   params = {'hash': element, 'hash_type': 'md5', 'email': emailApi, 'code': secret} 
@@ -23,20 +20,28 @@ for element in hashList:
   # añada el email y el código de su regirto en lugar de emailApi y de secret
   x = requests.post(url, data = params)
   clearHashList.append(x.content)
-  index += 1
-  outputClearHashes.write(str(index) + " " + x.content + "\n") 
+  if x.content == "":
+    plain.write((" " + "\n"))
+    print('Elemento guardado --> ' + x.content + "\n")
+  else:
+    plain.write(x.content)
+    print('Elemento guardado --> ' + x.content)
 
-outputClearHashes.close() 
+plain.close() 
 
 
-outputClearHashesToSHA = open("outputClearHashesToSHA.txt","w") 
+new_passwords = open("new_passwords.txt","w") 
 outputSalts = open("outputSalts.txt","w") 
-index2 = 0
 for element in clearHashList:
-    index2 += 1
     salt = bcrypt.gensalt(12)
     hashedSha = hashlib.sha512(element.encode('utf-8') + salt.encode('utf-8')).hexdigest()
-    outputClearHashesToSHA.write(str(index2) + " " + hashedSha + "\n")
-    outputSalts.write(str(index2) + " " + salt + "\n")
-outputClearHashesToSHA.close()
+    if element == "":
+        new_passwords.write(" " + "\n")
+        outputSalts.write(" " + "\n")
+    else:
+        new_passwords.write(hashedSha + "\n")
+        outputSalts.write(salt + "\n")
+new_passwords.close()
 outputSalts.close()
+
+
